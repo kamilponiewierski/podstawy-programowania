895df931930065700ac0492ad6d7234f464d562e
kponiewierski <vaskkr@gmail.comclear>
2020-10-15 17:54:15 +0200

    showcasing difference between operators task added

---
+++ difference_between_logical_and_bitwise_operators.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    // printf() displays the string inside quotation
+    printf("&& %d, & %d\n", 15 && 7, 15 & 7);
+    printf("|| %d, | %d", 15 || 0, 15 | 0);
+
+    return 0;
+}
