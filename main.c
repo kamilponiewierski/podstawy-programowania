3eb8e1b5e6df33fb61e6c7a3ce27c1b0a9a84c44
kponiewierski <vaskkr@gmail.comclear>
2020-12-10 20:15:57 +0100

    example list usage added in main

--- main.c
+++ main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
+#include <malloc.h>
+#include "labs_11/list.h"
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+    T* range = malloc(5 * sizeof(T));
+    for (int i = 0; i < 5; i++) {
+        *(range + i) = i;
+    }
+    List* list = constructListFromRange(range, range+4);
+
+    for(int i = 0; i< 5; i++) {
+        pop_front(list);
+        printList(list);
+        printf("\n");
+    }
+
+    List* copy = cloneList(list);
+
+    destruct(list);
+    destruct(copy);
+    free(range);
 }
